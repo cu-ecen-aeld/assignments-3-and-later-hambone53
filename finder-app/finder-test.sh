db8aa36568caa19bb93ce8d0d8bbb264304d3de8
Ryan Hamor <rhamor53@gmail.com>
2024-01-15 14:52:08 -0600

    modify finder-test to work with qemu rootfs

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -8,7 +8,7 @@ set -u
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
-username=$(cat conf/username.txt)
+username=$(cat /etc/finder-app/conf/username.txt)
 
 if [ $# -lt 3 ]
 then
@@ -31,40 +31,36 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 
 rm -rf "${WRITEDIR}"
 
-# create $WRITEDIR if not assignment1
-assignment=`cat ../conf/assignment.txt`
+mkdir -p "$WRITEDIR"
 
-if [ $assignment != 'assignment1' ]
+#The WRITEDIR is in quotes because if the directory path consists of spaces, then variable substitution will consider it as multiple argument.
+#The quotes signify that the entire string in WRITEDIR is a single string.
+#This issue can also be resolved by using double square brackets i.e [[ ]] instead of using quotes.
+if [ -d "$WRITEDIR" ]
 then
-	mkdir -p "$WRITEDIR"
-
-	#The WRITEDIR is in quotes because if the directory path consists of spaces, then variable substitution will consider it as multiple argument.
-	#The quotes signify that the entire string in WRITEDIR is a single string.
-	#This issue can also be resolved by using double square brackets i.e [[ ]] instead of using quotes.
-	if [ -d "$WRITEDIR" ]
-	then
-		echo "$WRITEDIR created"
-	else
-		exit 1
-	fi
+	echo "$WRITEDIR created"
+else
+	exit 1
 fi
 
 for i in $( seq 1 $NUMFILES)
 do
-	./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
-OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
+OUTPUTSTRING=$(finder.sh "$WRITEDIR" "$WRITESTR")
 
 # remove temporary directories
 rm -rf /tmp/aeld-data
 
-set +e
-echo ${OUTPUTSTRING} | grep "${MATCHSTR}"
-if [ $? -eq 0 ]; then
-	echo "success"
-	exit 0
-else
-	echo "failed: expected  ${MATCHSTR} in ${OUTPUTSTRING} but instead found"
-	exit 1
-fi
+echo ${OUTPUTSTRING} > "/tmp/assignment4-result.txt"
+
+# set +e	# unsets that shell script error out whenever command does
+# echo ${OUTPUTSTRING} | grep "${MATCHSTR}"
+# if [ $? -eq 0 ]; then
+# 	echo "success" > "/tmp/assignment4-result.txt"
+# 	exit 0
+# else
+# 	echo "failed: expected  ${MATCHSTR} in ${OUTPUTSTRING} but instead found" > "/tmp/assignment4-result.txt"
+# 	exit 1
+# fi
